34d02bda9ae500b9080c54a82c7fb9ecc01a878c
Tomasz Baslyk <tomasz.baslyk@visibacare.com>
2023-11-21 09:25:56 +0100

    Add basic move validation

--- shove/lib/shove_game.dart
+++ shove/lib/shove_game.dart
@@ -11,16 +11,45 @@ class ShoveGame {
 
   var board = List<List<ShoveSquare>>.generate(
       row,
-      (i) => List<ShoveSquare>.generate(col, (index) => ShoveSquare(col, row, null),
+      (i) => List<ShoveSquare>.generate(
+          col, (index) => ShoveSquare(col, row, null),
           growable: false),
       growable: false);
+
+  bool validateMove(ShoveSquare oldSquare, ShoveSquare newSquare) {
+    if (oldSquare.piece == null) {
+      return false;
+    }
+
+    if (newSquare.x > 7 ||
+        newSquare.x < 0 ||
+        newSquare.y > 7 ||
+        newSquare.y < 0) {
+      return false;
+    }
+
+    // todo: validate piece-specific logic here
+    if (oldSquare.piece!.pieceType != PieceType.shover) {
+      return false;
+    }
+
+    if ((oldSquare.x - newSquare.x).abs() > 1 ||
+        (oldSquare.y - newSquare.y).abs() > 1) {
+      return false;
+    }
+
+    if (board[newSquare.y][newSquare.x].piece != null) {
+      return false;
+    }
+
+    return true;
+  }
 
   ShoveGame()
       : pieces = List.generate(
             16,
             (index) => ShovePiece(
                 PieceType.shover, Image.asset('assets/textures/shover.png'))) {
-
     for (int currentCol = 0; currentCol < col; currentCol++) {
       board[0][currentCol].piece = pieces[currentCol];
       board[7][currentCol].piece = pieces[8 + currentCol];
---
+++ shove/test/shove_game_test.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shove/piece_type.dart';
+import 'package:shove/shove_game.dart';
+import 'package:shove/shove_piece.dart';
+import 'package:shove/shove_square.dart';
+
+void main() {
+  group('Validation tests', () {
+    test('Move should pass validation given a valid move', () {
+      final sut = ShoveGame();
+
+      var oldSquare = ShoveSquare(
+          1, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+      var newSquare = ShoveSquare(
+          2, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+
+      var result = sut.validateMove(oldSquare, newSquare);
+
+      expect(result, true);
+    });
+
+    test(
+        'Move should fail validation given a piece alredy exists on the new square',
+        () {
+      final sut = ShoveGame();
+
+      sut.board[2][2].piece =
+          ShovePiece(PieceType.shover, Image.network("placeholder"));
+
+      var oldSquare = ShoveSquare(
+          1, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+      var newSquare = ShoveSquare(
+          2, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+
+      var result = sut.validateMove(oldSquare, newSquare);
+
+      expect(result, false);
+    });
+
+    test('Move should fail validation given it is out ouf bounds', () {
+      final sut = ShoveGame();
+
+      var oldSquare = ShoveSquare(
+          7, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+      var newSquare = ShoveSquare(
+          8, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+
+      var result = sut.validateMove(oldSquare, newSquare);
+
+      expect(result, false);
+    });
+
+    test('Move should fail validation given piece is missing', () {
+      final sut = ShoveGame();
+
+      var oldSquare = ShoveSquare(1, 2, null);
+      var newSquare = ShoveSquare(
+          2, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+
+      var result = sut.validateMove(oldSquare, newSquare);
+
+      expect(result, false);
+    });
+
+    test('Move should fail validation given it moves too many squares', () {
+      final sut = ShoveGame();
+
+      var oldSquare = ShoveSquare(
+          1, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+      var newSquare = ShoveSquare(
+          3, 2, ShovePiece(PieceType.shover, Image.network("placeholder")));
+
+      var result = sut.validateMove(oldSquare, newSquare);
+
+      expect(result, false);
+    });
+  });
+}
