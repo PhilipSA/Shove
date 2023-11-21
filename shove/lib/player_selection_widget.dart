3f4bc73f5682baff2375506cf9c5b6628895a7f1
Markus <Markuslokander97@gmail.com>
2023-11-21 11:00:13 +0100

    added player selection widget for writing name.

---
+++ shove/lib/player_selection_widget.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:shove/cellula/cellula_foundation/cellula_tokens.dart';
+import 'package:shove/cellula/cellula_foundation/components/cellula_button.dart';
+import 'package:shove/cellula/cellula_foundation/components/cellula_textinput.dart';
+import 'package:shove/shove_game.dart';
+import 'package:shove/ui/game_board_widget/shove_board_widget.dart';
+
+class PlayersWidget extends StatefulWidget {
+  const PlayersWidget({super.key});
+
+  @override
+  State<PlayersWidget> createState() => _PlayersWidgetState();
+}
+
+class _PlayersWidgetState extends State<PlayersWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        CellulaTextInput(
+          isRequired: true,
+          maxLength: 12,
+          errorText: 'Player one name is required',
+          cellulaTokens: CellulaTokens.none(),
+          placeholderText: 'Enter player one',
+          onChanged: (String) {},
+        ),
+        CellulaTextInput(
+          isRequired: true,
+          maxLength: 12,
+          errorText: 'Player two is requried',
+          cellulaTokens: CellulaTokens.none(),
+          placeholderText: 'Enter player two',
+          onChanged: (String) {},
+        ),
+        CellulaButton(
+          buttonVariant: CellulaButtonVariant.primary(
+              CellulaTokens.none(), CellulaButtonSize.xLarge),
+          text: 'Start Game',
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ShoveBoardWidget(
+                          game: ShoveGame(),
+                        )));
+          },
+        )
+      ],
+    ));
+  }
+}
--- shove/lib/ui/start_play_widget.dart
+++ shove/lib/ui/start_play_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shove/cellula/cellula_foundation/cellula_tokens.dart';
 import 'package:shove/cellula/cellula_foundation/components/cellula_button.dart';
+import 'package:shove/player_selection_widget.dart';
 import 'package:shove/ui/game_board_widget/shove_board_widget.dart';
 import 'package:shove/shove_game.dart';
 
@@ -14,12 +15,8 @@ class PlayButton extends StatelessWidget {
           CellulaTokens.none(), CellulaButtonSize.xLarge),
       text: 'Play',
       onPressed: () {
-        Navigator.push(
-            context,
-            MaterialPageRoute(
-                builder: (context) => ShoveBoardWidget(
-                      game: ShoveGame(),
-                    )));
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const PlayersWidget()));
       },
     );
   }
