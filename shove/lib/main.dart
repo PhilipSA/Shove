e2366f03655d546e31d172e519759f77886fc421
Markus <Markuslokander97@gmail.com>
2023-11-21 18:53:13 +0100

    added music to main and music file.

--- shove/lib/main.dart
+++ shove/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:shove/ui/start_screen.widget.dart';
 
@@ -31,6 +32,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    _playAudio();
+  }
+
+  Future<void> _playAudio() async {
+    try {
+      await player.play(DeviceFileSource(
+          'assets/sounds/music/Action_2.mp3')); // will immediately start playing
+    } catch (e) {
+      print("Error playing audio: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,5 +58,11 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: const StartScreen(),
     );
+  }
+
+  @override
+  void dispose() {
+    player.stop();
+    super.dispose();
   }
 }
