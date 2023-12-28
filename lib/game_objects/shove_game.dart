66b49fe330061c9d351f680ec90290212a18b645
Philip S <philip.ppp@live.se>
2023-12-28 15:17:03 +0100

    Disable evaluator cache because the AI cheated

--- lib/game_objects/shove_game.dart
+++ lib/game_objects/shove_game.dart
@@ -371,8 +371,8 @@ class ShoveGame {
           getSquareByXY(aiMove.oldSquare.x, aiMove.oldSquare.y)!,
           getSquareByXY(aiMove.newSquare.x, aiMove.newSquare.y)!,
           currentPlayersTurn,
-          throwerSquare: getSquareByXY(
-              aiMove.throwerSquare?.x ?? -1, aiMove.throwerSquare?.y ?? -1));
+          throwerSquare: getSquareByXY(aiMove.throwerSquare?.x ?? -9999,
+              aiMove.throwerSquare?.y ?? -9999));
 
       final audioToPlay = move(convertIsolatedAiMoveToActualMove);
       return audioToPlay;
